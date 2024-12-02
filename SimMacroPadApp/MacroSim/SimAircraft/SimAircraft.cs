f8a937be70136de96b790bc07a5164148705bd64
John Taylor <jkvato@gmail.com>
2024-12-01 17:42:38 -0800

    Add SimAircraft classes

---
+++ SimMacroPadApp/MacroSim/SimAircraft/SimAircraft.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroSim.SimAircraft;
+
+internal class SimAircraft
+{
+   public string Title { get; set; }
+   public bool HasPanelLight { get; set; }
+   public bool HasPedestralLight { get; set; }
+   public bool HasCabinLight { get; set; }
+   public bool HasTaxiLight { get; set; }
+   public bool HasLandingLight { get; set; }
+   public bool HasStrobeLight { get; set; }
+   public bool HasNavLight { get; set; }
+   public bool HasWinLight { get; set; }
+   public bool HasBeaconLight { get; set; }
+   public bool HasGlareshieldLight { get; set; }
+   public bool HasLogoLight { get; set; }
+   public bool HasRecognitionLight { get; set; }
+
+   public SimAircraft()
+   {
+      Title = string.Empty;
+   }
+}
---
+++ SimMacroPadApp/MacroSim/SimAircraft/SimAircraftCollection.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroSim.SimAircraft;
+
+internal class SimAircraftCollection : IList<SimAircraft>
+{
+   private readonly List<SimAircraft> aircraftList = new List<SimAircraft>();
+
+   public SimAircraft this[int index] { get => ((IList<SimAircraft>)aircraftList)[index]; set => ((IList<SimAircraft>)aircraftList)[index] = value; }
+
+   public int Count => ((ICollection<SimAircraft>)aircraftList).Count;
+
+   public bool IsReadOnly => ((ICollection<SimAircraft>)aircraftList).IsReadOnly;
+
+   public void Add(SimAircraft item)
+   {
+      ((ICollection<SimAircraft>)aircraftList).Add(item);
+   }
+
+   public void Clear()
+   {
+      ((ICollection<SimAircraft>)aircraftList).Clear();
+   }
+
+   public bool Contains(SimAircraft item)
+   {
+      return ((ICollection<SimAircraft>)aircraftList).Contains(item);
+   }
+
+   public void CopyTo(SimAircraft[] array, int arrayIndex)
+   {
+      ((ICollection<SimAircraft>)aircraftList).CopyTo(array, arrayIndex);
+   }
+
+   public IEnumerator<SimAircraft> GetEnumerator()
+   {
+      return ((IEnumerable<SimAircraft>)aircraftList).GetEnumerator();
+   }
+
+   public int IndexOf(SimAircraft item)
+   {
+      return ((IList<SimAircraft>)aircraftList).IndexOf(item);
+   }
+
+   public void Insert(int index, SimAircraft item)
+   {
+      ((IList<SimAircraft>)aircraftList).Insert(index, item);
+   }
+
+   public bool Remove(SimAircraft item)
+   {
+      return ((ICollection<SimAircraft>)aircraftList).Remove(item);
+   }
+
+   public void RemoveAt(int index)
+   {
+      ((IList<SimAircraft>)aircraftList).RemoveAt(index);
+   }
+
+   IEnumerator IEnumerable.GetEnumerator()
+   {
+      return ((IEnumerable)aircraftList).GetEnumerator();
+   }
+}
